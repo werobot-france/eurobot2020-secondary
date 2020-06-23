7625778e7d9f049c0ad861527c4bb8645b5ead58
Le_Futuriste <spamfree@matthieubessat.fr>
2020-06-23 14:26:01 +0200

    fix: add node pty as dependency and add proper auto connect bluetooth process exit

--- node/auto_connect_bluetooth.js
+++ node/auto_connect_bluetooth.js
@@ -1,18 +1,23 @@
-var pty = require('node-pty');
- 
-var ptyProcess = pty.spawn("bluetoothctl", [], {
+const process = require('process') 
+const pty = require('node-pty')
+const address = "DC:0C:2D:5F:09:F8"
+const ptyProcess = pty.spawn("bluetoothctl", [], {
   name: 'xterm-color',
   cols: 80,
   rows: 30,
   cwd: process.env.HOME,
   env: process.env
-});
- 
+})
 ptyProcess.on('data', function(data) {
-  process.stdout.write(data);
-});
- 
-ptyProcess.write('devices\n');
+  process.stdout.write(data)
+  if (data.indexOf('Connection successful') !== -1) {
+    console.log()
+    console.log('Connection is considered as successful')
+    ptyProcess.kill()
+    process.exit()
+  }
+})
+ptyProcess.write('devices\n')
+ptyProcess.write('connect ' + address + '\n')
 
-ptyProcess.write('connect DC:0C:2D:5F:09:F8\n');
 
